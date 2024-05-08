e964b866aca87485bd9ef05524c49aff1a6d9426
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-05-08 13:10:42 +0430

    Fix variable name in addToCart function

--- data/cart.js
+++ data/cart.js
@@ -5,7 +5,7 @@ export function addToCart(productId) {
   let matchingItem;
   cart.forEach((cartItem) => {
     if (productId === cartItem.productId) {
-      matchingItem = item;
+      matchingItem = cartItem;
     }
   });
 
